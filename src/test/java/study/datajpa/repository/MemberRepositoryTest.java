7b0f901e2fe5429ea44f3356e0a016d3263470b4
ryouzhe <sjh3183@naver.com>
2022-01-07 13:46:02 +0900

    > Web 확장성 (Domain Class Convert) > Web 확장성 (페이징 & 소트)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -261,4 +261,10 @@ class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+
+    }
 }
